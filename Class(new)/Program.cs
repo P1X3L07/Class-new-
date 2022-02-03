0a3dc43590b8a31878316ee881a4e2642154fcc7
P1X3L07 <patricialowis2@gmail.com>
2022-02-03 17:49:31 +0700

    Add project files.

---
+++ Class(new)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program 
+{
+    class Students
+    {
+        public string name = "Kate";
+        public static string otherName = "Kat";
+        public string grade;
+
+        public Students()
+        {
+            grade = Console.ReadLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Students theStudent = new Students();
+        Console.Write(theStudent.name);
+        Students theGrade = new Students();
+        Console.Write(theGrade.grade);
+
+        Console.Write(Students.otherName);
+    }
+}
